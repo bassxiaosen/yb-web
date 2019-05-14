714fa62227b707a8c8526d74a1159faf185a4dea
BassXiaoSen <314121378@qq.com>
2019-05-14 15:07:14 +0800

    切换地址

--- src/utils/lurl.js
+++ src/utils/lurl.js
@@ -1,2 +1,3 @@
-const lurl = 'http://119.29.121.40:9999'
+// const lurl = 'http://119.29.121.40:9999'
+const lurl = 'http://120.78.0.52:9999'
 export default lurl
--- src/utils/url.js
+++ src/utils/url.js
@@ -1,2 +1,3 @@
-const url = 'http://119.29.121.40:8081'
+// const url = 'http://119.29.121.40:8081'
+const url = 'http://120.78.0.52:8081'
 export default url
